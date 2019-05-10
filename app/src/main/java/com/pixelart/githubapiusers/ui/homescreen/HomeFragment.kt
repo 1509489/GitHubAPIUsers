bed4d94f5083e92e4a0eea486f70cc3d3d519bd4
Justice <popystone@gmail.cm>
2019-05-10 23:48:41 +0100

    Added implementation to allow user to sign in using google authentication

--- app/src/main/java/com/pixelart/githubapiusers/ui/homescreen/HomeFragment.kt
+++ app/src/main/java/com/pixelart/githubapiusers/ui/homescreen/HomeFragment.kt
@@ -2,22 +2,19 @@ package com.pixelart.githubapiusers.ui.homescreen
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.pixelart.githubapiusers.R
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var rootView: View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        return rootView
     }
-
-
 }
