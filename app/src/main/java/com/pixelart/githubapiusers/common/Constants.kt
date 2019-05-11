74769307de9cc23a415640119096779427575c21
Justice <popystone@gmail.cm>
2019-05-11 10:33:00 +0100

    Added required dependencies and generated dto objects from json

---
+++ app/src/main/java/com/pixelart/githubapiusers/common/Constants.kt
@@ -0,0 +1,2 @@
+package com.pixelart.githubapiusers.common
+
--- app/src/main/java/com/pixelart/githubapiusers/ui/usersscreen/UsersFragment.kt
+++ app/src/main/java/com/pixelart/githubapiusers/ui/usersscreen/UsersFragment.kt
@@ -1,4 +1,4 @@
-package com.pixelart.githubapiusers.ui.homescreen
+package com.pixelart.githubapiusers.ui.usersscreen
 
 
 import android.os.Bundle
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.pixelart.githubapiusers.R
 
-class HomeFragment : Fragment() {
+class UsersFragment : Fragment() {
 
     private lateinit var rootView: View
 
