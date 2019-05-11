b6006ee5502fecd610ffce32afcee59d4cd4f5b4
Justice <popystone@gmail.cm>
2019-05-11 12:01:42 +0100

    added dependency injection for the all user repository

--- app/src/main/java/com/pixelart/githubapiusers/di/application/ApplicationComponent.kt
+++ app/src/main/java/com/pixelart/githubapiusers/di/application/ApplicationComponent.kt
@@ -3,6 +3,6 @@ package com.pixelart.githubapiusers.di.application
 import com.pixelart.githubapiusers.di.network.NetworkModule
 import dagger.Component
 
-@Component(modules = [NetworkModule::class])
+@Component(modules = [NetworkModule::class, ApplicationModule::class])
 interface ApplicationComponent {
 }
