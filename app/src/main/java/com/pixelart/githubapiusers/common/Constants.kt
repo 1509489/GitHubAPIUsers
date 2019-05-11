0e46946453855493ebad2f7f7a85e7b9316a3c9d
Justice <popystone@gmail.cm>
2019-05-11 10:55:06 +0100

    Added dependency dagger module implementation for the network service

--- app/src/main/java/com/pixelart/githubapiusers/common/Constants.kt
+++ app/src/main/java/com/pixelart/githubapiusers/common/Constants.kt
@@ -1,3 +1,5 @@
 package com.pixelart.githubapiusers.common
 
 const val BASE_URL = "https://api.github.com/"
+
+const val OKHTTP_CONNECTION_TIMEOUT = 30L
---
+++ app/src/main/java/com/pixelart/githubapiusers/di/application/ApplicationScope.kt
@@ -0,0 +1,7 @@
+package com.pixelart.githubapiusers.di.application
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScope
