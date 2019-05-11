220ae057eea27b1a62e6fa15258e77cb0037232a
Justice <popystone@gmail.cm>
2019-05-11 12:42:12 +0100

    added implementation to inject the alluserviewmodel in the users fragment

--- app/src/main/java/com/pixelart/githubapiusers/di/application/ApplicationComponent.kt
+++ app/src/main/java/com/pixelart/githubapiusers/di/application/ApplicationComponent.kt
@@ -1,9 +1,12 @@
 package com.pixelart.githubapiusers.di.application
 
+import com.pixelart.githubapiusers.di.fragment.FragmentComponent
+import com.pixelart.githubapiusers.di.fragment.FragmentModule
 import com.pixelart.githubapiusers.di.network.NetworkModule
 import dagger.Component
 
 @ApplicationScope
 @Component(modules = [NetworkModule::class, ApplicationModule::class])
 interface ApplicationComponent {
+    fun newFragmentComponent(fragmentModule: FragmentModule):FragmentComponent
 }
--- app/src/main/java/com/pixelart/githubapiusers/ui/usersscreen/UsersFragment.kt
+++ app/src/main/java/com/pixelart/githubapiusers/ui/usersscreen/UsersFragment.kt
@@ -6,15 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.pixelart.githubapiusers.AppController
 import com.pixelart.githubapiusers.R
+import com.pixelart.githubapiusers.di.fragment.FragmentModule
+import javax.inject.Inject
 
 class UsersFragment : Fragment() {
+    @Inject lateinit var viewModel: AllUsersViewModel
 
     private lateinit var rootView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val fragmentComponent = (activity?.application as AppController)
+            .applicationComponent
+            .newFragmentComponent(FragmentModule(this))
+        fragmentComponent.injectUsersScreen(this)
+
+        viewModel.getAllUsers()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_home, container, false)
         return rootView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getUsersLiveData().observe(this, Observer {
+
+        })
     }
 }
