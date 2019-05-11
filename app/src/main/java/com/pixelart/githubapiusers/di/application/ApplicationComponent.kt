0dcbfa1a4984adcc5498b1a87977f17100fb6952
Justice <popystone@gmail.cm>
2019-05-11 11:06:37 +0100

    Added a response class to handle success or failure responses

---
+++ app/src/main/java/com/pixelart/githubapiusers/di/application/ApplicationComponent.kt
@@ -0,0 +1,8 @@
+package com.pixelart.githubapiusers.di.application
+
+import com.pixelart.githubapiusers.di.network.NetworkModule
+import dagger.Component
+
+@Component(modules = [NetworkModule::class])
+interface ApplicationComponent {
+}
